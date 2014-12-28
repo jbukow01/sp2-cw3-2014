0ec17128d045cebcbb29da82e1f8a2c75d5fdc37
jbukow01 <jbukow01@dcs.bbk.ac.uk>
2014-12-28 18:31:38 +0000

    randomFloor method added

--- src/cw3/Runner.java
+++ src/cw3/Runner.java
@@ -23,15 +23,15 @@ public class Runner {
 		customerBase = new ArrayList<Customer>();
 
 		createCustomerBase(numberOfCustomers, numberOfFloors);
-		createBuildingStructure(customerBase, numberOfFloors, startingFloor);
+		createBuildingStructure(customerBase, numberOfFloors);
 
 		building.elevator.bottomToTopMove(numberOfFloors, 4);
 	}
 
-	private static void createCustomerBase(int maxCustomers, int maxFloors) {
+	private static void createCustomerBase(int maxCustomers, int numberOfFloors) {
 		for (int i = 0; i < maxCustomers; i++) {
-			Customer cust = new Customer((int) (Math.random() * maxFloors),
-					(int) (Math.random() * maxFloors));
+			Customer cust = new Customer((int) (Math.random() * numberOfFloors),
+					(int) (Math.random() * numberOfFloors));
 			if (cust.getStartingFloor() != cust.getDesiredFloor()) {
 				customerBase.add(cust);
 			}
@@ -39,7 +39,17 @@ public class Runner {
 	}
 
 	private static void createBuildingStructure(
-			ArrayList<Customer> customerBase, int maxFloors, int startingFloor) {
-		building = new Building(customerBase, maxFloors, startingFloor);
+			ArrayList<Customer> customerBase, int maxFloors) {
+		building = new Building(customerBase, maxFloors, randomFloor(0,
+				numberOfFloors));
+	}
+
+	public static int randomFloor(int start, int end) {
+		int range = (end - start) + 1;
+		int floor = start + (int) (Math.random() * range);
+		if (floor == 13) {
+			floor++;
+		}
+		return floor;
 	}
 }
